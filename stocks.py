6cb6cc5b21768932d6c1424533c1389b7af510ba
baylorwroblewski <114018346+baylorwroblewski@users.noreply.github.com>
2023-01-26 09:12:53 -0500

    Stocks V2

--- stocks.py
+++ stocks.py
@@ -2,19 +2,22 @@
 #CS175-02
 #stocks
 
-amountP = 40.00
-commissionPP = 0.03
-amountS = 42.75
-commissionPS = 0.03
-numberS = 2000.00
+#receives input from user
+amountP = float(input("Please enter the amount paid per stock: "))
+amountS = float(input("Please enter the amount each stock sold for: "))
+commission = float(input("Please enter the commission rate: "))
+numberS = float(input("Please enter the number of stock bought: "))
 
+#calculations
 amountP = numberS*amountP
-commissionP = amountP*commissionPP
+commissionP = amountP*commission
 amountS = numberS*amountS
-commissionPS = amountS*commissionPS
+commissionPS = amountS*commission
 profitS = amountP + commissionP + commissionPS
 profit = amountS - profitS
 
+#prints out calculations
+print(" ")
 print("Amount paid for the stock: ${:,.2f}".format(amountP))
 print("Commission paid on the purchase: ${:,.2f}".format(commissionP))
 print("Amount the stock sold for: ${:,.2f}".format(amountS))
