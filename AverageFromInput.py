f9bc542eb0310365a72c3816e21126c389e5c167
baylorwroblewski <114018346+baylorwroblewski@users.noreply.github.com>
2023-03-15 15:58:36 -0400

    Average From Input w/ Exception Handling

--- AverageFromInput.py
+++ AverageFromInput.py
@@ -1,19 +1,48 @@
 # Baylor Wroblewski
 # CS 175 L
-# Average From Input
+# Average From Input w/ Exception Handling
 
-def main():
-    file_object = open('numbers.txt' ,'r')
+def read_numbers(file_name):
+    objects = []
+    with open(file_name, 'r') as file:
+        for line in file:
+            number = float(line.strip())
+            objects.append(number)
+    return objects
+
+def calculate_average(objects):
+    average = sum(objects) / len(objects)
+    return average
+
+def print_average(average):
+    try:
+        print("Average:", average)
+        
+    except FileNotFoundError:
+        print("Error: File not found")
+        return None
+    except ValueError:
+        print("Error: Invalid value in file")
+        return None
+    
+#Not sure if needed
+def print_numbers(objects):
     x = 0
     total = 0
-    for nums in file_object:
-        x = x + 1
+    for nums in objects:
         amount = float(nums)
         total = total + amount
+        x = x + 1
         print(f"I read in {x} number(s) "f"Current number is:     {amount:.1f}"f"  Total is: {total:.1f}")
-    file_object.close()
-    
-    average = total / x
-    print (f"Average: {average:.1f}")
-    
-main()
+
+def main():
+
+    file_name = 'numbers.txt'
+    objects = read_numbers(file_name)
+    print_numbers(objects)
+    average = calculate_average(objects)
+    print_average(average)
+
+if __name__ == '__main__':
+    main()
+
